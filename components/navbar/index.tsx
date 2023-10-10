1ca95c348b38755ee32bacfe8ebf9621902c1aef
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-10-10 09:47:05 +0900

    don't show "home" link on navbar

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import { Routes } from "@/lib/constants";
 
@@ -10,23 +11,27 @@ export const Navbar = () => {
 	return (
 		<Container className="relative z-[100] border-b bg-white py-8">
 			<div className="flex items-center justify-between lg:gap-24">
-				<Image
-					src="/images/logo.svg"
-					alt="Arch"
-					width={77}
-					height={32}
-					className="md:h-10 md:w-24"
-				/>
+				<Link href={Routes.Home}>
+					<Image
+						src="/images/logo.svg"
+						alt="Arch"
+						width={77}
+						height={32}
+						className="md:h-10 md:w-24"
+					/>
+				</Link>
 				<nav className="flex items-center md:grow">
 					<div className="md:hidden">
 						<MobileNav />
 					</div>
 					<div className="hidden md:flex md:grow md:justify-evenly lg:justify-start lg:gap-16">
-						{Object.entries(Routes).map(([name, path]) => (
-							<NavLink key={path} href={path}>
-								{name}
-							</NavLink>
-						))}
+						{Object.entries(Routes)
+							.filter(([name]) => name !== "Home")
+							.map(([name, path]) => (
+								<NavLink key={path} href={path}>
+									{name}
+								</NavLink>
+							))}
 					</div>
 				</nav>
 			</div>
--- components/navbar/mobile-navbar.tsx
+++ components/navbar/mobile-navbar.tsx
@@ -12,6 +12,8 @@ import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
 export const MobileNav = () => {
 	const [isDialogOpen, setIsDialogOpen] = useState(false);
 
+	if (typeof window !== "undefined" && window.screen.width >= 768) return <></>;
+
 	return (
 		<Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
 			<DialogTrigger>
@@ -22,15 +24,17 @@ export const MobileNav = () => {
 				)}
 			</DialogTrigger>
 			<DialogContent className="inset-x-0 top-24 flex w-full max-w-none translate-x-0 translate-y-0 flex-col gap-4 bg-[#EEEFF4] px-10 py-12 data-[state=closed]:slide-out-to-right-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-right-1/2 data-[state=open]:slide-in-from-top-[48%] [&>button]:hidden">
-				{Object.entries(Routes).map(([name, path]) => (
-					<Link
-						key={path}
-						href={path}
-						className="text-3xl font-bold text-[#1B1D23]"
-					>
-						{name}
-					</Link>
-				))}
+				{Object.entries(Routes)
+					.filter(([name]) => name !== "Home")
+					.map(([name, path]) => (
+						<Link
+							key={path}
+							href={path}
+							className="text-3xl font-bold text-[#1B1D23]"
+						>
+							{name}
+						</Link>
+					))}
 			</DialogContent>
 		</Dialog>
 	);
