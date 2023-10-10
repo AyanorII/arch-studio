252dadfd536790b3bef44fab782e5a6a2d475334
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-10-10 11:06:38 +0900

    fix vertical text positioning

--- components/pages/vertical-text.tsx
+++ components/pages/vertical-text.tsx
@@ -4,9 +4,9 @@ type Props = {
 
 export const VerticalText = ({ children }: Props) => {
 	return (
-		<div className="absolute left-[8%] top-0 hidden w-0 flex-col items-center min-[1200px]:flex">
-			<div className="mb-28 h-[104px] w-[1px] bg-[#C8CCD8]" />
-			<span className="rotate-90 text-lg font-medium uppercase tracking-[18px] text-[#C8CCD8]">
+		<div className="absolute left-[8%] top-0 hidden h-full w-0 flex-col items-center min-[1200px]:flex">
+			<div className="h-[104px] w-[1px] bg-[#C8CCD8]" />
+			<span className="ml-6 mt-5 block h-0 w-0 origin-bottom-left rotate-90 text-lg font-medium uppercase tracking-[18px] text-[#C8CCD8]">
 				{children}
 			</span>
 		</div>
