e220f10aea4214020c04854c6fc5f352f30c72a3
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-10-10 14:14:48 +0900

    add tailwind form components

---
+++ components/ui/textarea.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+export interface TextareaProps
+	extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+	({ className, ...props }, ref) => {
+		return (
+			<textarea
+				className={cn(
+					"flex min-h-[80px] w-full aria-[invalid='true']:border-red-500 border-b focus-visible:border-slate-600 transition-[border] border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
+					className
+				)}
+				ref={ref}
+				{...props}
+			/>
+		);
+	}
+);
+Textarea.displayName = "Textarea";
+
+export { Textarea };
