a078529523f5acd3b725a4e97795f3dcaaecff57
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2023-10-10 13:15:53 +0900

    fix footer spacing

--- components/pages/footer.tsx
+++ components/pages/footer.tsx
@@ -11,7 +11,7 @@ import { Container, buttonVariants } from "../ui";
 export const Footer = () => {
 	return (
 		<Container className="p-0 md:max-w-none md:p-0 lg:relative lg:px-8">
-			<footer className="relative mt-16 flex w-full flex-col items-center bg-[#EEEFF4] pb-10 pt-24 md:static md:w-[80%] md:flex-row md:items-center md:gap-10 md:p-0 lg:mt-0 lg:w-[90%] lg:gap-20">
+			<footer className="relative mt-16 flex w-full flex-col items-center bg-[#EEEFF4] pb-10 pt-24 md:static md:w-[80%] md:flex-row md:gap-10 md:p-0 lg:mt-0 lg:w-[90%] xl:gap-20">
 				<Link
 					href={Routes.Home}
 					className="absolute -top-16 grid place-items-center bg-[#1B1D23] px-8 py-12 md:static lg:h-[200px] lg:w-[200px]"
